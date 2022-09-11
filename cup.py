c1a6f07ce0960dfa16766dd76e92857240a5770f
d.stobbe <d.stobbe@campus.tu-berlin.de>
2022-09-11 21:40:24 +0200

    name and price fn

--- cup.py
+++ cup.py
@@ -1,4 +1,4 @@
-class Topping:
+class Combo:
     def __init__(self,new,old) -> None:
         self.new = new
         self.old = old
@@ -6,24 +6,39 @@ class Topping:
     def __str__(self) -> str:
         return str(self.old)+' with '+str(self.new)
 
+    def price(self):
+        return self.old.price() + self.new.price()
+
+    def name(self):
+        return replace_last_with_with_and(str(self))
+
 
 class Cupcake():
     def __str__(self) -> str:
         return "🧁"
 
-    def add(other):
-        return [other]
+    def price(self):
+        return 1
+
 
 def Chocolate(cake):
-    return Topping(_Chocolate(),cake)
-
+    return Combo(_Chocolate(),cake)
 class _Chocolate():
     def __str__(self) -> str:
         return "🍫"
 
-def Peanuts(cake):
-    return Topping(_Peanuts(),cake)
+    def price(self):
+        return .1
 
+def Peanuts(cake):
+    return Combo(_Peanuts(),cake)
 class _Peanuts:
     def __str__(self) -> str:
         return "🥜"
+
+    def price(self):
+        return .2
+
+def replace_last_with_with_and(s):
+    li = s.rsplit('with', 1)
+    return 'and'.join(li)
--- cup_test.py
+++ cup_test.py
@@ -5,13 +5,13 @@ from cup import *
 
 def test_cupcakename():
     item = Cupcake()
-
     assert str(item) == "🧁"
 
-# def test_chocolate():
-#     item = Chocolate()
-
-#     assert cupcakename(item) == "🍫"
 
 def test_cupcake_with_chocolate_and_peanuts():
-    assert str(Peanuts(Chocolate(Cupcake()))) == "🧁 with 🍫 with 🥜"
+    assert Peanuts(Chocolate(Cupcake())).name() == "🧁 with 🍫 and 🥜"
+
+def test_price():
+    Chocolate(Cupcake()).price() == 1.1
+
+    Peanuts(Chocolate(Cupcake())).price() == 1.3
