8f31c57367ee1cc4123897cc40005c9ab3dbef31
d.stobbe <d.stobbe@campus.tu-berlin.de>
2022-09-11 13:28:13 +0200

    classes

--- cup.py
+++ cup.py
@@ -1,16 +1,18 @@
 
 class Cupcake:
-
-
     def __str__(self) -> str:
         return "cupcake"
 
 class Chocolate:
+    def __init__(self, bakery):
+        pass
+
     def __str__(self) -> str:
         return "chocolate"
     
 
 def cupcakename(item):
+    item = str(item)
     if item == "cupcake":
         return "🧁"
 
--- cup_test.py
+++ cup_test.py
@@ -12,3 +12,7 @@ def test_chocolate():
     item = Chocolate()
 
     assert cupcakename(str(item)) == "🍫"
+
+def test_cupcakewithchocolate():
+
+    assert cupcakename(Chocolate(Cupcake())) == "🧁 with 🍫"
